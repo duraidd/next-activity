0b3d198cee6d594066e463751dc55d3a6c1dde47
duraidd <duraiessakimuthu@gmail.com>
2021-02-27 12:26:47 +0530

    Initial commit

---
+++ app/src/main/java/com/netcom/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.netcom.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view=findViewById(R.id.textView);
+        view.setText("Anjukuttan");
+    }
+}
